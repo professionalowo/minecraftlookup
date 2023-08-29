0d707d69de31f2211f161af9aafc1bd25f640d5f
professionalowo <martin.brandl1307@gmail.com>
2023-08-29 15:12:53 +0200

    added content to about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,9 +1,18 @@
-function About(){
-    return(
-        <div>
-            <h1>About</h1>
+function About() {
+    return (
+        <div className="w-screen h-screen flex flex-wrap justify-center align-middle items-center">
+            <div className="w-fit h-fit bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h1 className="block text-gray-700 text-2xl font-bold mb-2">About</h1>
+                <div>
+                    <p>Hello</p>
+                    <p>This is my first project using react + typescript</p>
+                    <p>Please post suggestions at my GitHub</p>
+                </div>
+                <p>Look at the <a href="https://github.com/professionalowo/minecraftlookup/tree/master"
+                                  className="hover:text-blue-600 text-gray-600 font-bold">source</a>!</p>
+            </div>
         </div>
     )
 }
 
-export default  About
+export default About
